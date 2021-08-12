a23c812a99ebe774ea7286a8b17ea107f4729650
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-12 19:22:37 -0230

    Aug 12 7:22 pm commit

--- app.js
+++ app.js
@@ -1,26 +1,104 @@
-import express, { json } from 'express';
+/*import express, { json } from 'express';
 import pkg from 'pg';
-const { Pool } = pkg;
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import session from 'express-session';
+import methodOverride from 'method-override';
+import flash from 'express-flash';
+import passport from 'passport';
 import { MongoClient } from 'mongodb';
 import nunjucks from 'nunjucks';
 import passportLocal from 'passport-local';
-const app = express();
+import Pool from 'pg-pool';
+import route from 'router'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import initPassport from '../passport-config';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+*/
+
+//SERVER SIDE
+
+const express = require('express');
+const pkg = require ('pg');
+const path = require('path');
+const cookieParser = require ('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const flash = require('express-flash');
+const passport = require('passport');
+const { MongoClient } = require('mongodb');
+const nunjucks = require('nunjucks');
+const passportLocal = require('passport-local');
+const Pool = require('pg-pool');
+const { fileURLToPath } = require('url');
+const initPassport = require('./passport-config');
+
+
+
+
+
 const port = 9000;
 
-/*MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("animaldb");
-    dbo.createCollection("Animals2", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-  });
-  */
 
- nunjucks.configure({
-   express: app
- })
+const getUserByName = (name) => {
+  return global.registeredUsers.find(user => user.name === name);
+};
+const getUserById = (id) => {
+  return global.registeredUsers.find(user => user.id === id);
+};
+
+initPassport(passport, getUserByName, getUserById);
+
+/*
+const indexRouter = import('../routes/index');
+const usersRouter = import('../routes/users');
+const signupRouter = import('../routes/signup');
+const signinRouter = import('../routes/signin');
+const signoutRouter = import('../routes/signout');
+*/
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const signupRouter = require('./routes/signup');
+const signinRouter = require('./routes/signin');
+const signoutRouter = require('./routes/signout');
+const searchRouter = require('./routes/search')
+
+const app = express();
+
+nunjucks.configure('views', {
+  noCache: true,
+  autoescape: true,
+  express: app
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'unsafe value',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+app.use(flash());
+
+
+const pool = new Pool({
+  user: 'sprint',
+  host: 'localhost',
+  database: "animaldb",
+  password: "password",
+  port: 5432
+});
 
   async function main() {
     const uri = "mongodb+srv://sprintTeam:password1001@cluster0.oq1gf.mongodb.net/Cluster0?retryWrites=true&w=majority";
@@ -28,7 +106,7 @@ const port = 9000;
     try {
       await client.connect();
       
-      await findOneListingsByAnimalName(client, ""); 
+    await findOneListingsByAnimalName(client, ""); 
     }catch(err) {
       console.error(err);
     } finally {
@@ -46,30 +124,25 @@ const port = 9000;
 // }
 // }
   
-main().catch(console.error);
+    main().catch(console.error);
 
 
-const pool = new Pool({
-    user: 'sprint',
-    host: 'localhost',
-    database: "animaldb",
-    htmlpassword: "password",
-    port: 5432
-});
 
-app.use(express.json());
+const checkSignedIn = (req, res, next) => {
+  if(req.isAuthenticated()) { // if they are signed in
+      return next(); // proceed to next middleware
+  }
+  res.redirect('/signin'); // else, redirect them to the signin page
+};
 
-/*async function findManyListingsByAnimalName(client, nameOfListing) {
-	const result = await client.db("animaldb").collection("animals").findOne({ AnimalName: nameOfListing });
-	if(result){
-		console.log(`Found a listing in the collection with the name '${nameOfListing}'`)
-		console.log(result);
-	}else{
-		console.log(`No listings found with the name '${nameOfListing}'`)
-	}
-}*/
+const checkNotSignedIn = (req, res, next) => {
+  if(req.isAuthenticated()) { // if they are signed in
+      return res.redirect('/'); // send them to the home page since they are signed in
+  }
+  next(); // else, allow them to visit the page they want to
+};
 
-const query = "";
+const query = "red";
 async function findOneListingsByAnimalName(client, nameOfListing) {
 	const result = await client.db("animaldb").collection("animals").find({AnimalName: new RegExp(query)}).toArray();
 	if(result){
@@ -80,10 +153,34 @@ async function findOneListingsByAnimalName(client, nameOfListing) {
 	}
 }
 
+app.use('/', indexRouter);
+app.use('/users', checkSignedIn, usersRouter);
+app.use('/signup', checkNotSignedIn, signupRouter);
+app.use('/signin', checkNotSignedIn, signinRouter);
+app.use('/signout', signoutRouter);
+app.use('/search', searchRouter)
+
 
 
 
 app.listen(9000, () => {
 	console.log(`Listening on http://localhost:${port}`);
 });
-  
+
+  var PGVar = new Promise(function (respond, reject) {
+  return pool.query(`SELECT * FROM mock_data`, function (err, result){
+    if (err) reject(err);
+    respond(result);
+    console.log(result.rows)
+
+  });
+});
+/*
+function btnwork(){
+  getElementById("myBtn").addEventListener("click", myFunction)
+ function myFunction() {
+ query = getElementById("searchy")
+ console.log("query")
+ 
+ }
+ */
