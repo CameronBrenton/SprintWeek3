3bcadc7aa4835fa69699c85ef896ab0dfa5fab11
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-17 20:33:29 -0230

    Commented out the users variable in the data object, we don't want to display that stuff. Fixed up the post method so that it works correctly and we can sign in! :)

--- routes/signup.js
+++ routes/signup.js
@@ -16,36 +16,32 @@ const data = {
   layout:  'layout.njk',
   title: 'Signup',
   pages: global.pages,
-  users: global.registeredUsers
+  //users: global.registeredUsers
 };
 
 router.get('/', function(req, res) {
   res.render('signup.njk', data);
 });
 
-router.post('/', async function(req, res) {
-  const { userid, email,  password } = req.body;
+router.post('/', async function (req, res) {
+  const { userid, email, password } = req.body;
   const encryptedPassword = await bcrypt.hash(password, 10)
 
   const id = Date.now().toString();
   const newUser = { id, userid, email, password };
-  
+
   // model.updateUser -> db....
- /* 
-  global.registeredUsers.push(newUser);
-  if(global.passwords === undefined) {
-      global.passwords = {};
-  }
-  global.passwords[email] = encryptedPassword;
-  */
+
+
+
   let results = await pool.query('SELECT * FROM users where email = $1', [email]);
   if (results.rows.length > 0) {
     res.send("error! there is already an account with this email");
   } else {
     let insert_result = await pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, encryptedPassword]);
-    res.send("created account!");
-  res.render('users.njk', data);
+    res.render('signin.njk', data);
   }
+  
 });
 
 module.exports = router;
