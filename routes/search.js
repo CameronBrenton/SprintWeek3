6f463240060a0693ba8a31f3b5a7b25307745fdc
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-20 08:26:33 -0230

    Added data variable to display info

--- routes/search.js
+++ routes/search.js
@@ -5,9 +5,16 @@ const router = express.Router();
 const passport = require('passport');
 const fetch = require("node-fetch")
 
+const data = {
+	message: 'Go ahead and make a search!',
+	layout: 'layout.njk',
+	title: 'Search',
+	pages: global.pages,
+	//users: global.registeredUsers
+};
 
 router.get('/', function (req, res) {
-	res.render('search.njk')
+	res.render('search.njk', data)
 });
 
 module.exports = router;
