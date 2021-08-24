ab559d2f243b465f9cccbfbda36b59d1c1d185b8
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-24 15:11:05 -0230

    Removed unused requirements, added comments, removed space.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const pkg = require ('pg');
 const path = require('path');
 const cookieParser = require ('cookie-parser');
 const logger = require('morgan');
@@ -7,15 +6,13 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 const flash = require('express-flash');
 const passport = require('passport');
-const { MongoClient } = require('mongodb');
 const nunjucks = require('nunjucks');
-const passportLocal = require('passport-local');
 const Pool = require('pg-pool');
-const { fileURLToPath } = require('url');
 const initPassport = require('./passport-config');
 
 const port = 9000;
 
+// connect to postgres
 const pool = new Pool({
 	user: 'sprint',
 	host: 'localhost',
@@ -24,26 +21,18 @@ const pool = new Pool({
 	port: 5432
 });
 
+// Get user email in database
 const getUserByEmail = async (email) => {
-  //return global.registeredUsers.find(user => user.name === name);
-  //return animaldb.registeredUsers.find(user => user.email === email);
-  // Write a query call to the databse that returns the user with the given email
   const results = await pool.query('SELECT * FROM users where email = $1', [email]);
-  // Check to see how many rows were returned.
-  //If one, perfect, return the object representing that row.
-  //If none, return null or something.
- 
   if(results.rows.length === 1) {
     return results.rows[0];
   }else{
     return null;
   }
 };
-
+// Get user id in database
 const getUserById = async (userid) => {
-  //return global.registeredUsers.find(user => user.id === id);
   const results = await pool.query("SELECT * FROM users WHERE userid = $1", [userid]);
-
   if(results.rows.length === 1) {
     return results.rows[0];
   }else{
@@ -51,6 +40,7 @@ const getUserById = async (userid) => {
   }
 };
 
+// initialize passport
 initPassport(passport, getUserByEmail, getUserById);
 
 const indexRouter = require('./routes/index');
@@ -68,6 +58,7 @@ nunjucks.configure('views', {
   express: app
 });
 
+// setting up middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -83,35 +74,29 @@ app.use(passport.session());
 app.use(methodOverride('_method'));
 app.use(flash());
 
-
-
+// Authenticate user
 const checkSignedIn = (req, res, next) => {
-  if(req.isAuthenticated()) { // if they are signed in
-      return next(); // proceed to next middleware
+  if(req.isAuthenticated()) { 
+      return next(); 
   }
-  res.redirect('/signin'); // else, redirect them to the signin page
+  res.redirect('/signin'); 
 };
 
+// check if user is signed in
 const checkNotSignedIn = (req, res, next) => {
-  if(req.isAuthenticated()) { // if they are signed in
-      return res.redirect('/'); // send them to the home page since they are signed in
+  if(req.isAuthenticated()) { 
+      return res.redirect('/'); 
   }
-  next(); // else, allow them to visit the page they want to
+  next(); 
 };
 
-
+// set up routes and pass in middlewares
 app.use('/', indexRouter);
 app.use('/signup', checkNotSignedIn, signupRouter);
 app.use('/signin', checkNotSignedIn, signinRouter);
 app.use('/signout', checkSignedIn, signoutRouter);
 app.use('/search', checkSignedIn, searchRouter);
 app.use('/submit', submitRouter);
-
-
-
-
-
-
 
 app.listen(9000, () => {
 	console.log(`Listening on http://localhost:${port}`);
