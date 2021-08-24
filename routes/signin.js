1b1055d48b2194cf555d03e37c65a2af38410d02
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-24 15:22:54 -0230

    Removed uneeded requirements and added some comments

--- routes/signin.js
+++ routes/signin.js
@@ -1,23 +1,18 @@
-
-//import express from 'express';
-//import bcrypt from 'bcryptjs';
-const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
 const data = {
-	//message: 'Login to your account!',
 	layout: 'layout.njk',
 	title: 'Login',
-	pages: global.pages,
-	//users: global.registeredUsers
+	pages: global.pages
 };
 
 router.get('/', function (req, res) {
 	res.render('signin.njk', data);
 });
 
+// Login
 router.post('/', passport.authenticate('local', {
 	successRedirect: '/search',
 	failureRedirect: '/signin',
