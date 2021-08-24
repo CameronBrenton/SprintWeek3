94158cf065422e2f8f22e99c22775ce44b29a8f1
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-24 15:20:56 -0230

    Removed uneeded requirements and added some comments

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,15 @@
 
-const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const fetch = require("node-fetch")
 
 const data = {
 	message: 'Go ahead and make a search!',
 	layout: 'layout.njk',
 	title: 'Search',
-	pages: global.pages,
-	//users: global.registeredUsers
+	pages: global.pages
 };
 
+// Get the search page
 router.get('/', function (req, res) {
 	res.render('search.njk', data)
 });
