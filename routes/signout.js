50a4121895ab025293507b9f169e0f8b4744e022
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-24 15:23:52 -0230

    Removed pool config, unused requirements, and added comments.

--- routes/signout.js
+++ routes/signout.js
@@ -1,15 +1,5 @@
-//import express from 'express';
 const express = require('express');
 const router = express.Router();
-const Pool = require('pg-pool');
-
-const pool = new Pool({
-  user: 'sprint',
-  host: 'localhost',
-  database: "animaldb",
-  password: "password",
-  port: 5432
-});
 
 let data = {
   message: 'Signed out of your account!',
@@ -22,16 +12,11 @@ router.get('/', (req, res) => {
   res.render('signout.njk', data);
 });
 
-
-
-
+// Sign out of the application
 router.post('/', async function (req, res) {
-  //global.loggedInUser = undefined;
-  //res.render('index.njk', data);
   req.logOut();
   req.user = undefined;
   res.redirect('/signin');
 });
-
 
 module.exports = router;
